554c019b107f09805d832f81e63ee56fabe13793
SonuAkhtar <sonua981@gmail.com>
2023-11-23 14:16:15 +0530

    updated styles

--- src/testData.js
+++ src/testData.js
@@ -154,4 +154,252 @@ export const testData = [
       ],
     },
   },
+  {
+    id: 4,
+    month: "May",
+    year: "2023",
+    type: "Fixed",
+    time: "1:52 PM",
+    salary: [
+      { name: "Salary", amount: 10000, datetime: "12 Nov, 2:34 PM" },
+      { name: "HD-FC", amount: 9000, datetime: "13 Nov, 2:34 PM" },
+    ],
+    spent: {
+      fixed: {
+        rd: 30000,
+        sip: 10000,
+      },
+      highExp: [
+        { name: "dad", amount: 10000, datetime: "12 Nov, 2:34 PM" },
+        { name: "Sahil", amount: 9000, datetime: "13 Nov, 2:34 PM" },
+        { name: "Delhi", amount: 8000, datetime: "14 Nov, 2:34 PM" },
+        { name: "Clothes", amount: 4000, datetime: "15 Nov, 2:34 PM" },
+      ],
+      others: [
+        { amount: 30, datetime: "23 Oct, 1:23 PM" },
+        { amount: 40, datetime: "24 Oct, 1:23 PM" },
+        { amount: 50, datetime: "25 Oct, 1:23 PM" },
+        { amount: 60, datetime: "26 Oct, 1:23 PM" },
+        { amount: 70, datetime: "27 Oct, 1:23 PM" },
+        { amount: 80, datetime: "28 Oct, 1:23 PM" },
+      ],
+    },
+  },
+  {
+    id: 4,
+    month: "May",
+    year: "2023",
+    type: "Fixed",
+    time: "1:52 PM",
+    salary: [
+      { name: "Salary", amount: 10000, datetime: "12 Nov, 2:34 PM" },
+      { name: "HD-FC", amount: 9000, datetime: "13 Nov, 2:34 PM" },
+    ],
+    spent: {
+      fixed: {
+        rd: 30000,
+        sip: 10000,
+      },
+      highExp: [
+        { name: "dad", amount: 10000, datetime: "12 Nov, 2:34 PM" },
+        { name: "Sahil", amount: 9000, datetime: "13 Nov, 2:34 PM" },
+        { name: "Delhi", amount: 8000, datetime: "14 Nov, 2:34 PM" },
+        { name: "Clothes", amount: 4000, datetime: "15 Nov, 2:34 PM" },
+      ],
+      others: [
+        { amount: 30, datetime: "23 Oct, 1:23 PM" },
+        { amount: 40, datetime: "24 Oct, 1:23 PM" },
+        { amount: 50, datetime: "25 Oct, 1:23 PM" },
+        { amount: 60, datetime: "26 Oct, 1:23 PM" },
+        { amount: 70, datetime: "27 Oct, 1:23 PM" },
+        { amount: 80, datetime: "28 Oct, 1:23 PM" },
+      ],
+    },
+  },
+  {
+    id: 4,
+    month: "May",
+    year: "2023",
+    type: "Fixed",
+    time: "1:52 PM",
+    salary: [
+      { name: "Salary", amount: 10000, datetime: "12 Nov, 2:34 PM" },
+      { name: "HD-FC", amount: 9000, datetime: "13 Nov, 2:34 PM" },
+    ],
+    spent: {
+      fixed: {
+        rd: 30000,
+        sip: 10000,
+      },
+      highExp: [
+        { name: "dad", amount: 10000, datetime: "12 Nov, 2:34 PM" },
+        { name: "Sahil", amount: 9000, datetime: "13 Nov, 2:34 PM" },
+        { name: "Delhi", amount: 8000, datetime: "14 Nov, 2:34 PM" },
+        { name: "Clothes", amount: 4000, datetime: "15 Nov, 2:34 PM" },
+      ],
+      others: [
+        { amount: 30, datetime: "23 Oct, 1:23 PM" },
+        { amount: 40, datetime: "24 Oct, 1:23 PM" },
+        { amount: 50, datetime: "25 Oct, 1:23 PM" },
+        { amount: 60, datetime: "26 Oct, 1:23 PM" },
+        { amount: 70, datetime: "27 Oct, 1:23 PM" },
+        { amount: 80, datetime: "28 Oct, 1:23 PM" },
+      ],
+    },
+  },
+  {
+    id: 4,
+    month: "May",
+    year: "2023",
+    type: "Fixed",
+    time: "1:52 PM",
+    salary: [
+      { name: "Salary", amount: 10000, datetime: "12 Nov, 2:34 PM" },
+      { name: "HD-FC", amount: 9000, datetime: "13 Nov, 2:34 PM" },
+    ],
+    spent: {
+      fixed: {
+        rd: 30000,
+        sip: 10000,
+      },
+      highExp: [
+        { name: "dad", amount: 10000, datetime: "12 Nov, 2:34 PM" },
+        { name: "Sahil", amount: 9000, datetime: "13 Nov, 2:34 PM" },
+        { name: "Delhi", amount: 8000, datetime: "14 Nov, 2:34 PM" },
+        { name: "Clothes", amount: 4000, datetime: "15 Nov, 2:34 PM" },
+      ],
+      others: [
+        { amount: 30, datetime: "23 Oct, 1:23 PM" },
+        { amount: 40, datetime: "24 Oct, 1:23 PM" },
+        { amount: 50, datetime: "25 Oct, 1:23 PM" },
+        { amount: 60, datetime: "26 Oct, 1:23 PM" },
+        { amount: 70, datetime: "27 Oct, 1:23 PM" },
+        { amount: 80, datetime: "28 Oct, 1:23 PM" },
+      ],
+    },
+  },
+  {
+    id: 4,
+    month: "May",
+    year: "2023",
+    type: "Fixed",
+    time: "1:52 PM",
+    salary: [
+      { name: "Salary", amount: 10000, datetime: "12 Nov, 2:34 PM" },
+      { name: "HD-FC", amount: 9000, datetime: "13 Nov, 2:34 PM" },
+    ],
+    spent: {
+      fixed: {
+        rd: 30000,
+        sip: 10000,
+      },
+      highExp: [
+        { name: "dad", amount: 10000, datetime: "12 Nov, 2:34 PM" },
+        { name: "Sahil", amount: 9000, datetime: "13 Nov, 2:34 PM" },
+        { name: "Delhi", amount: 8000, datetime: "14 Nov, 2:34 PM" },
+        { name: "Clothes", amount: 4000, datetime: "15 Nov, 2:34 PM" },
+      ],
+      others: [
+        { amount: 30, datetime: "23 Oct, 1:23 PM" },
+        { amount: 40, datetime: "24 Oct, 1:23 PM" },
+        { amount: 50, datetime: "25 Oct, 1:23 PM" },
+        { amount: 60, datetime: "26 Oct, 1:23 PM" },
+        { amount: 70, datetime: "27 Oct, 1:23 PM" },
+        { amount: 80, datetime: "28 Oct, 1:23 PM" },
+      ],
+    },
+  },
+  {
+    id: 4,
+    month: "May",
+    year: "2023",
+    type: "Fixed",
+    time: "1:52 PM",
+    salary: [
+      { name: "Salary", amount: 10000, datetime: "12 Nov, 2:34 PM" },
+      { name: "HD-FC", amount: 9000, datetime: "13 Nov, 2:34 PM" },
+    ],
+    spent: {
+      fixed: {
+        rd: 30000,
+        sip: 10000,
+      },
+      highExp: [
+        { name: "dad", amount: 10000, datetime: "12 Nov, 2:34 PM" },
+        { name: "Sahil", amount: 9000, datetime: "13 Nov, 2:34 PM" },
+        { name: "Delhi", amount: 8000, datetime: "14 Nov, 2:34 PM" },
+        { name: "Clothes", amount: 4000, datetime: "15 Nov, 2:34 PM" },
+      ],
+      others: [
+        { amount: 30, datetime: "23 Oct, 1:23 PM" },
+        { amount: 40, datetime: "24 Oct, 1:23 PM" },
+        { amount: 50, datetime: "25 Oct, 1:23 PM" },
+        { amount: 60, datetime: "26 Oct, 1:23 PM" },
+        { amount: 70, datetime: "27 Oct, 1:23 PM" },
+        { amount: 80, datetime: "28 Oct, 1:23 PM" },
+      ],
+    },
+  },
+  {
+    id: 4,
+    month: "May",
+    year: "2023",
+    type: "Fixed",
+    time: "1:52 PM",
+    salary: [
+      { name: "Salary", amount: 10000, datetime: "12 Nov, 2:34 PM" },
+      { name: "HD-FC", amount: 9000, datetime: "13 Nov, 2:34 PM" },
+    ],
+    spent: {
+      fixed: {
+        rd: 30000,
+        sip: 10000,
+      },
+      highExp: [
+        { name: "dad", amount: 10000, datetime: "12 Nov, 2:34 PM" },
+        { name: "Sahil", amount: 9000, datetime: "13 Nov, 2:34 PM" },
+        { name: "Delhi", amount: 8000, datetime: "14 Nov, 2:34 PM" },
+        { name: "Clothes", amount: 4000, datetime: "15 Nov, 2:34 PM" },
+      ],
+      others: [
+        { amount: 30, datetime: "23 Oct, 1:23 PM" },
+        { amount: 40, datetime: "24 Oct, 1:23 PM" },
+        { amount: 50, datetime: "25 Oct, 1:23 PM" },
+        { amount: 60, datetime: "26 Oct, 1:23 PM" },
+        { amount: 70, datetime: "27 Oct, 1:23 PM" },
+        { amount: 80, datetime: "28 Oct, 1:23 PM" },
+      ],
+    },
+  },
+  {
+    id: 4,
+    month: "May",
+    year: "2023",
+    type: "Fixed",
+    time: "1:52 PM",
+    salary: [
+      { name: "Salary", amount: 10000, datetime: "12 Nov, 2:34 PM" },
+      { name: "HD-FC", amount: 9000, datetime: "13 Nov, 2:34 PM" },
+    ],
+    spent: {
+      fixed: {
+        rd: 30000,
+        sip: 10000,
+      },
+      highExp: [
+        { name: "dad", amount: 10000, datetime: "12 Nov, 2:34 PM" },
+        { name: "Sahil", amount: 9000, datetime: "13 Nov, 2:34 PM" },
+        { name: "Delhi", amount: 8000, datetime: "14 Nov, 2:34 PM" },
+        { name: "Clothes", amount: 4000, datetime: "15 Nov, 2:34 PM" },
+      ],
+      others: [
+        { amount: 30, datetime: "23 Oct, 1:23 PM" },
+        { amount: 40, datetime: "24 Oct, 1:23 PM" },
+        { amount: 50, datetime: "25 Oct, 1:23 PM" },
+        { amount: 60, datetime: "26 Oct, 1:23 PM" },
+        { amount: 70, datetime: "27 Oct, 1:23 PM" },
+        { amount: 80, datetime: "28 Oct, 1:23 PM" },
+      ],
+    },
+  },
 ];
